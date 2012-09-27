113050a91c6c28bf836c69172e187f77058def2e
Fred H <fredericohonorio@ua.pt>
2012-09-27 15:39:34 +0100

    Added constructor

--- t1/YUV.cpp
+++ t1/YUV.cpp
@@ -6,10 +6,37 @@
 
 YUV::YUV(int nR, int nC, int f, int t)
 {
+	nRows = nR;
+	nCols = nC;
+	fps = f;
+	type = t;
+	
+	switch (type)
+	{
+		case 444:
+			uvCols = nCols;
+			uvRows = nRows;
+			break;
+		case 422:
+			uvCols = nCols/2;
+			uvRows = nRows/2;
+			break;
+		case 420:
+			uvCols = nCols/2;
+			uvRows = nRows/2;
+			break;
+	}
+	
+	bufferSize = nRows * nCols + uvRows * uvCols * 2;
+	buffer = new unsigned char[bufferSize];
+	yBuffer = buffer;
+	uBuffer = buffer + nRows*nCols;
+	vBuffer = buffer + nRows*nCols + uvRows*uvCols;
 }
 
 YUV::YUV(char *filename)
 {
+	
 }
 
 YUV::~YUV()
