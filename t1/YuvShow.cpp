82bb291a69b0d2e164f5d0a060803f656620a079
Filipe Manco <filipe.manco@ua.pt>
2012-10-16 03:29:32 +0100

    Reimplement YuvShow using YUV
    Missing some functionality
    * -wait

---
+++ t1/YuvShow.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <cv.h>
+#include <highgui.h>
+#include "YUV.h"
+
+int main( int argc, char** argv )
+{
+	unsigned int fps; /* frames per second */
+	char inputKey = '?'; /* parse the pressed key */
+	int end = 0, playing = 1, loop = 0; /* control variables */
+
+	/* check for the mandatory arguments */
+	if( argc < 2 ) {
+		fprintf( stderr, "Usage: YuvShow filename\n" );
+		return 1;
+	}
+
+	/* Creating YUV */
+	YUV yuv(argv[argc - 1]);
+
+	/* Parse other command line arguments */
+	for(int n = 1 ; n < argc ; n++)
+	{
+		if(!strcmp("-fps", argv[n]))
+		{
+			fps = atof(argv[n+1]);
+			n++;
+
+			yuv.setFps(fps);
+		}
+
+		// FIXME: How to do this?
+		if(!strcmp("-wait", argv[n]))
+		{
+			playing = 0;
+		}
+
+		if(!strcmp("-loop", argv[n]))
+		{
+			loop = 1;
+		}
+	}
+
+	while(!end)
+	{
+		if (!yuv.readFrame()) {
+			yuv.displayFrame();
+		} else if (loop) {
+			yuv.rewind();
+		} else {
+			end = true;
+			break;
+		}
+	}
+
+	return 0;
+}
