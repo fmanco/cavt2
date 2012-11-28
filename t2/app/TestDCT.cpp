3863dfdfadb410352ee24a2b83f2f8d510287fa5
Fred H <fredericohonorio@ua.pt>
2012-11-28 21:07:44 +0000

    DCT and quantization implemented. TODO: refactor to use YUV data.

--- t2/app/TestDCT.cpp
+++ t2/app/TestDCT.cpp
@@ -11,10 +11,38 @@ static float PI_8 = atan(1)/2; // pi/8
 inline float s(int i) { return (i==0) ? S_0 : S_X; }; //scaling factor
 inline int round(float x) { return (int) (x>0.0f) ? floor(x+0.5f) : ceil(x-0.5f);};
 
-void shift(int* block);
+//JPEG defines 8x8 quantizing matrices 
+static float yQuantizeMatrix[8*8] = {
+	16, 11, 10, 16, 24, 40, 51, 61,
+	12, 12, 14, 19, 26, 58, 60, 55,
+	14, 13, 16, 24, 40, 57, 69, 56,
+	14, 17, 22, 29, 51, 87, 80, 62,
+	18, 22, 37, 56, 68,109,103, 77,
+	24, 35, 55, 64, 81,104,113, 92,
+	49, 64, 78, 87,103,121,120,101,
+	72, 92, 95, 98,112,100,103, 99,
+};
+
+static float uvQuantizeMatrix[8*8] = {
+	17, 18, 24, 47, 99, 99, 99, 99,
+	18, 21, 26, 66, 99, 99, 99, 99,
+	24, 26, 56, 99, 99, 99, 99, 99,
+	47, 66, 99, 99, 99, 99, 99, 99,
+	99, 99, 99, 99, 99, 99, 99, 99,
+	99, 99, 99, 99, 99, 99, 99, 99,
+	99, 99, 99, 99, 99, 99, 99, 99,
+	99, 99, 99, 99, 99, 99, 99, 99,
+};
+
+
+void shift(int* block, int down);
 void print(int* block);
 void print(float* block);
+void dct(int* values, float* dct);
+void invdct(float* dct, int* values);
 void quantize(float* dct, float factor, int* quantizedDct);
+void dequantize(int* quantizedDct, float factor, float* dct);
+void diff(int* a, int* b, int* result);
 
 int main( int argc, char** argv ){
 	int block[BLOCK_SIZE*BLOCK_SIZE]= {
@@ -28,10 +56,69 @@ int main( int argc, char** argv ){
 		87, 79, 69, 68, 65, 76, 78, 94,
 	};
 
-	shift(block);
+	float dctCoefs[BLOCK_SIZE*BLOCK_SIZE] = {0.0f};
+	int qdct[BLOCK_SIZE*BLOCK_SIZE] = {0};
+	int block_[BLOCK_SIZE*BLOCK_SIZE] = {0};
+
+	printf("Block:\n");
 	print(block);
 
-	float dct[BLOCK_SIZE*BLOCK_SIZE] = {0.0f};
+	shift(block, 0);
+	printf("Shift:\n");
+	print(block);
+
+	dct(block, dctCoefs);
+	printf("DCT (coefficients):\n");
+	print(dctCoefs);
+
+	quantize(dctCoefs, 1.0f, qdct);
+	printf("Quantize:\n");
+	print(qdct);
+
+	dequantize(qdct, 1.0f, dctCoefs);
+	printf("Dequantize:\n");
+	print(dctCoefs);
+
+	invdct(dctCoefs, block_);
+	printf("Inverse DCT:\n");
+	print(block_);
+
+	diff(block, block_, block_);
+	printf("Difference:\n");
+	print(block_);
+
+	// shift(block_, 1);
+	// printf("Unshift:\n");
+	// print(block_);	
+
+	return 0;
+}
+
+void diff(int* a, int* b, int* result){
+	for (int i = 0; i < 8; i++){
+		for (int j = 0; j < 8; j++){
+			result[i*8+j] = a[i*8+j] - b[i*8+j];
+		}
+	}
+}
+
+void invdct(float* dct, int* values){
+	float sum;
+
+	for (int i = 0; i < BLOCK_SIZE; i++){
+		for (int j = 0; j< BLOCK_SIZE; j++){
+			sum = 0;
+			for (int u = 0; u<BLOCK_SIZE; u++){		
+				for (int v = 0; v < BLOCK_SIZE; v++){
+					sum += s(u)*s(v)*dct[u*BLOCK_SIZE+v]*cos(PI_8*(i+0.5f)*u)*cos(PI_8*(j+0.5f)*v);
+				}
+			}
+			values[i*BLOCK_SIZE+j] = round(sum);
+		}
+	}	
+}
+
+void dct(int* values, float* dct){
 	float sum;
 
 	for (int u = 0; u<BLOCK_SIZE; u++){
@@ -39,40 +126,28 @@ int main( int argc, char** argv ){
 			sum = 0;
 			for (int i = 0; i < BLOCK_SIZE; i++){
 				for (int j = 0; j< BLOCK_SIZE; j++){
-					sum += s(u)*s(v)*block[i*BLOCK_SIZE+j]*cos(PI_8*(i+0.5f)*u)*cos(PI_8*(j+0.5f)*v);
+					sum += s(u)*s(v)*values[i*BLOCK_SIZE+j]*cos(PI_8*(i+0.5f)*u)*cos(PI_8*(j+0.5f)*v);
 				}
 			}
-
 			dct[u*BLOCK_SIZE+v] = sum;
+		}
+	}
+}
 
+
+void dequantize(int* qdct, float factor, float* dct){
+	float* matrix = yQuantizeMatrix;
+
+	for (int i = 0; i < 8; i++){
+		for (int j = 0; j < 8; j++){
+			dct[i*8+j] = qdct[i*8+j]*(matrix[i*8+j]*factor);
 		}
 	}
 
-	print(dct);
-
-	int qdct[BLOCK_SIZE*BLOCK_SIZE] = {0};
-
-	quantize(dct, 1.0f, qdct);
-
-	print(qdct);
-
-	return 0;
 }
 
 void quantize(float* dct, float factor, int* qdct){
-	//JPEG defines 8x8 quantizing matrices 
-	static float qMatrix[8*8] = {
-		16, 11, 10, 16, 24, 40, 51, 61,
-		12, 12, 14, 19, 26, 58, 60, 55,
-		14, 13, 16, 24, 40, 57, 69, 56,
-		14, 17, 22, 29, 51, 87, 80, 62,
-		18, 22, 37, 56, 68,109,103, 77,
-		24, 35, 55, 64, 81,104,113, 92,
-		49, 64, 78, 87,103,121,120,101,
-		72, 92, 95, 98,112,100,103, 99,
-	};
-
-	float* matrix = qMatrix;
+	float* matrix = yQuantizeMatrix;
 
 	for (int i = 0; i < 8; i++){
 		for (int j = 0; j < 8; j++){
@@ -82,10 +157,12 @@ void quantize(float* dct, float factor, int* qdct){
 }
 
 
-void shift(int* block){
+void shift(int* block, int down=0){
+
+	int offset = (down==0) ? -128 : 128;
 
 	for (int i = 0; i < BLOCK_SIZE*BLOCK_SIZE; i++){
-		block[i] -=128;
+		block[i] += offset;
 	}
 }
 
