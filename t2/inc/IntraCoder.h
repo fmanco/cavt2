75369980bb419c7f0817edc104d6688c3050cba6
Fred H <fredericohonorio@ua.pt>
2012-11-29 14:55:02 +0000

    Fixed quantization defaults.

--- t2/inc/IntraCoder.h
+++ t2/inc/IntraCoder.h
@@ -23,16 +23,16 @@ public:
 	/**
 	 * Writes a bitsream header for a video.
 	 */
-	int static writeHeader(uint rows, uint cols, uint fps, uint type, uint predMode, uint quantY, uint quantU, uint quantV, BitStream& bs); //add mode!?!?!
+	int static writeHeader(uint rows, uint cols, uint fps, uint type, uint predMode, uint quantY, uint quantU, uint quantV, BitStream& bs);
 
-	int static encode(YuvFrame& frame, BitStream& bs, uint mode, uint quantY, uint quantU, uint quantV);
+	int static encode(YuvFrame& frame, BitStream& bs, uint mode = 0, uint quantY = 0, uint quantU = 0, uint quantV = 0);
 
 	/**
 	 * Reads a bitstream header for a video.
 	 */
 	int static readHeader(BitStream& bs, uint *rows, uint *cols, uint *fps, uint *type, uint *predMode, uint *quantY, uint *quantU, uint *quantV); 
 
-	int static decode(BitStream& bs, YuvFrame& frame, uint mode, uint quantY, uint quantU, uint quantV);
+	int static decode(BitStream& bs, YuvFrame& frame, uint mode = 0, uint quantY = 0, uint quantU = 0, uint quantV = 0);
 };
 
 //==============================================================================
