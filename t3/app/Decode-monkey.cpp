d556b9249f2894ac3d139117e3cd7bac6db627c9
Fred H <fredericohonorio@ua.pt>
2013-01-12 19:22:33 +0000

    Added apps por monkey encoding and decoding.

---
+++ t3/app/Decode-monkey.cpp
@@ -0,0 +1,90 @@
+//==============================================================================
+// Decode-llpred.cpp
+//
+// Authors : Filipe Manco <filipe.manco@ua.pt>
+// Authors : Frederico Honório <fredericohonorio@ua.pt>
+//==============================================================================
+
+#include "base.h"
+#include "SFWriter.h"
+#include "MonkeyCoder.h"
+#include "BitStream.h"
+#include "Golomb.h"
+#include <string>
+
+
+//==============================================================================
+
+static void usage ( int argc, char** argv );
+
+
+//==============================================================================
+
+int main ( int argc, char** argv )
+{
+	int golombM;
+	int quant;
+
+	if (argc != 3) {
+		usage(argc, argv);
+		return -1;
+	}
+
+	std::string finname(argv[1]);
+	std::string foutname(argv[2]);
+
+	BitStream bs(finname, BitStream::READ);
+
+	if (bs.open()) {
+		printf("Unable to open file %s for reading!\n", argv[1]);
+		return -1;
+	}
+
+	uint32_t frames;
+	uint32_t samplerate;
+	uint32_t channels;
+
+	bs.readBits(32, &frames);
+	bs.readBits(32, &samplerate);
+	bs.readBits(32, &channels);
+	bs.readBits(32, (uint32_t*)&golombM);
+	bs.readBits(32, (uint32_t*)&quant);
+
+	SFWriter sfw(foutname, frames, samplerate, channels);
+	MonkeyCoder pc(quant);
+
+	if (sfw.open()) {
+		printf("Unable to open file %s for writing!\n", argv[2]);
+		return -1;
+	}
+
+	while (true) {
+		int16_t sample[2];
+		int32_t diff[2];
+
+		if (Golomb::decode(golombM, &diff[0], bs))
+			break;
+
+		if (Golomb::decode(golombM, &diff[1], bs))
+			break;
+
+		pc.decode(diff, sample);
+		sfw.writeFrame(sample);
+	}
+
+	bs.close();
+	sfw.close();
+
+	printf("Done! Decoded %u frames.\n", frames);
+}
+
+
+//==============================================================================
+
+static void usage ( int argc, char** argv )
+{
+	printf("USAGE: %s <input.ll> <output.wav>\n", argv[0]);
+}
+
+
+// EOF =========================================================================
---
+++ t3/app/Encode-monkey.cpp
@@ -0,0 +1,104 @@
+//==============================================================================
+// Encode-llpred.cpp
+//
+// Authors : Filipe Manco <filipe.manco@ua.pt>
+// Authors : Frederico Honório <fredericohonorio@ua.pt>
+//==============================================================================
+
+#include "base.h"
+#include "SFReader.h"
+#include "MonkeyCoder.h"
+#include "BitStream.h"
+#include "Golomb.h"
+#include <string>
+#include <cstdlib>
+
+
+//==============================================================================
+
+#define GOLOMB_ENC_M 1024
+#define QUANTIZATION 1
+
+
+//==============================================================================
+
+static void usage ( int argc, char** argv );
+
+
+//==============================================================================
+
+int main ( int argc, char** argv )
+{
+	int golombM;
+	int quant;
+
+	if (argc != 3 && argc != 5) {
+		usage(argc, argv);
+		return -1;
+	}
+
+	if (argc == 5) {
+		golombM = std::atoi(argv[3]);
+		quant = std::atoi(argv[4]);
+	} else {
+		golombM = GOLOMB_ENC_M;
+		quant = QUANTIZATION;
+	}
+
+	std::string finname(argv[1]);
+	std::string foutname(argv[2]);
+
+	SFReader sfr(finname);
+	BitStream bs(foutname, BitStream::WRITE);
+
+	MonkeyCoder pc(quant);
+
+	if (sfr.open()) {
+		printf("Unable to open file %s for reading!\n", argv[1]);
+		return -1;
+	}
+
+	if (bs.open()) {
+		printf("Unable to open file %s for writing!\n", argv[2]);
+		return -1;
+	}
+
+	bs.writeBits((uint32_t)sfr.getNFrames(),    32);
+	bs.writeBits((uint32_t)sfr.getSamplerate(), 32);
+	bs.writeBits((uint32_t)sfr.getChannels(),   32);
+	bs.writeBits((uint32_t)golombM,             32);
+	bs.writeBits((uint32_t)quant,               32);
+
+	if (sfr.open()) {
+		printf("Unable to open file %s for reading!\n", argv[1]);
+		return -1;
+	}
+
+	for (uint32_t i = 0; i < sfr.getNFrames(); i++) {
+		int16_t sample[2];
+		int32_t diff[2];
+
+		sfr.nextFrame(sample);
+		pc.encode(sample, diff);
+
+		Golomb::encode(golombM, diff[0], bs);
+		Golomb::encode(golombM, diff[1], bs);
+	}
+
+	sfr.close();
+	bs.close();
+
+	printf("Done! Encoded %u frames.\n", sfr.getNFrames());
+}
+
+
+//==============================================================================
+
+static void usage ( int argc, char** argv )
+{
+	printf("USAGE: %s <input.wav> <output.ll> [<GolombM = %d> <Quant = %d>]\n",
+		argv[0], GOLOMB_ENC_M, QUANTIZATION);
+}
+
+
+// EOF =========================================================================
